de2a23417a79d6c4d6a56e1500593d7fc34e0a01
michiru <michiru.shigemitsu@gmail.com>
2024-06-23 13:09:43 +0900

    add:ModelSpecテスト項目追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,21 +1,20 @@
 class Task < ApplicationRecord
   validates :title, :content, :deadline_on, :priority, :status, presence: true
-  enum priority: [:low, :middle, :high]
-  enum status: [:waiting, :working, :completed]
-  
-  scope :search, -> (search_params,sort_deadline_on,sort_priority)do
-    status_search(search_params[:status])
-      .title_search(search_params[:title])
-      .latest_deadline(sort_deadline_on)
-      .highest_priority(sort_priority)
-      .normal_sort
-  end
+  enum priority: %i[low middle high]
+  enum status: %i[waiting working completed]
 
-  scope :status_search, ->(status){where(status: status) if status.present?}
-  scope :title_search, ->(part) {where("title like ?", "%#{part}%") if part.present?}
-  scope :latest_deadline, ->(sort_deadline_on)  {order(deadline_on: :asc) if sort_deadline_on}
-  scope :highest_priority, ->(sort_priority)  {order(priority: :desc) if sort_priority}
-  #デフォルトスコープみたいのがあるらしいけど・・
-  scope :normal_sort, -> {order(created_at: :desc)}  
-  
+  scope :search, lambda { |search_params, sort_deadline_on, sort_priority|
+                   status_search(search_params[:status])
+                     .title_search(search_params[:title])
+                     .latest_deadline(sort_deadline_on)
+                     .highest_priority(sort_priority)
+                     .normal_sort
+                 }
+
+  scope :status_search, ->(status) { where(status:) if status.present? }
+  scope :title_search, ->(part) { where('title like ?', "%#{part}%") if part.present? }
+  scope :latest_deadline, ->(sort_deadline_on) { order(deadline_on: :asc) if sort_deadline_on }
+  scope :highest_priority, ->(sort_priority) { order(priority: :desc) if sort_priority }
+  # デフォルトスコープみたいのがあるらしいけど・・
+  scope :normal_sort, -> { order(created_at: :desc) }
 end
