9c8d8a5402277966ed6f3a18180abdfe91f6f2f2
michiru <michiru.shigemitsu@gmail.com>
2024-06-27 18:25:33 +0900

    add: エラーページsample

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     resources :users
   end
   resources :sessions, only: %i[new create destroy]
+  get '*not_found' => 'application#routing_error'
+  post '*not_found' => 'application#routing_error'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
